cab854eef057f6c071f94b9ed54369ed9ae267c6
Stephanie McFarlane <stephanie.mcfarlane@avanade.com>
2022-10-19 16:52:29 +0100

    adding exercises

---
+++ booleans.py
@@ -0,0 +1,9 @@
+number1 = float(input('Enter first number:'))
+number2 = float(input('Enter second number:'))
+
+if number1 > number2:
+    number1bigger = True
+else:
+    number1bigger = False
+
+print('it is ', number1bigger, 'that number1 is bigger.')
---
+++ rounding.py
@@ -0,0 +1,8 @@
+number1 = input('Enter whole number:')
+number2 = input('Enter decimal number:')
+
+int_num = int(number1)
+float_num = float(number2)
+round_num = int(round(float_num))
+
+print(int_num, float_num, round_num)
